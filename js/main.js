204c48ad20a06b4306829449dbd4a5b20d224176
Alexey <sapeshnik@mail.ru>
2018-01-31 14:29:41 +0300

    add js h2

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ var test = document.getElementById('test');
 test.addEventListener('click', function(){
     alert('Click to H1!!!')
 })
+var test2 = document.getElementById('test2');
+test2.addEventListener('click', function(){
+    alert('Click to H2!!!')
+})
